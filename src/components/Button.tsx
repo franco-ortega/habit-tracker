a8f92d5f558bffffe4988015dbca9c68aefb4a25
Franco Ortega <francopdx@gmail.com>
2026-05-07 13:38:24 -0700

    finish Header element structure and styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ type Button = {
 
 export default function Button({ children }: Button) {
 	return (
-		<button className='bg-blue-500 text-white p-2 rounded hover:bg-blue-600'>
+		<button className='bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed'>
 			{children}
 		</button>
 	);
