1a53cf0a3381e38a6a9a28def71f44ca0b9d3de4
Franco Ortega <francopdx@gmail.com>
2026-05-11 15:51:50 -0700

    add optional disabled prop to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,15 @@ import type { ReactNode } from 'react';
 
 type Button = {
 	children: ReactNode;
+	disabled?: boolean;
 };
 
-export default function Button({ children }: Button) {
+export default function Button({ children, disabled = false }: Button) {
 	return (
-		<button className='bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed'>
+		<button
+			disabled={disabled}
+			className='bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed'
+		>
 			{children}
 		</button>
 	);
