a5923bfc718642a73b23eff2f28bac4416692899
Franco Ortega <francopdx@gmail.com>
2026-05-11 15:42:29 -0700

    install date fns

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,5 +1,7 @@
 // component to display list of habits that are mapped from the database
 import React from 'react';
+import Button from './Button';
+import { startOfWeek } from 'date-fns';
 // import { Habit } from '../types';
 
 type Habit = {
@@ -12,6 +14,25 @@ interface HabitListProps {
 	habits: Habit[];
 }
 
+function HabitItem({ habit }: { habit: Habit }) {
+	const visibleDates = [new Date(), new Date(), new Date()]; // Example: Show the last 3 days
+
+	return (
+		<li className='rounded-x1 bg-zinc-800 p-4 flex flex-col gap-3'>
+			<h3 className='flex items-center'>{habit.name}</h3>
+			<p className='flex items-center justify-between'>{habit.description}</p>
+			<div className='flex gap-1.5'>
+				{visibleDates.map((date) => (
+					<Button key={date.toISOString()}>
+						<span>Mon</span>
+						<span>2</span>
+					</Button>
+				))}
+			</div>
+		</li>
+	);
+}
+
 const HabitList: React.FC<HabitListProps> = ({ habits }) => {
 	if (habits.length === 0) {
 		return (
@@ -22,8 +43,8 @@ const HabitList: React.FC<HabitListProps> = ({ habits }) => {
 	}
 
 	return (
-		<div className='flex flex-col gap-3'>
-			<ul>
+		<div className='flex flex-col gap-3 rounded-x1'>
+			<ul className='flex flex-col gap-3'>
 				{habits.map((habit) => (
 					<HabitItem key={habit.id} habit={habit} />
 				))}
@@ -31,14 +52,5 @@ const HabitList: React.FC<HabitListProps> = ({ habits }) => {
 		</div>
 	);
 };
-
-function HabitItem({ habit }: { habit: Habit }) {
-	return (
-		<li className='rounded-x1 bg-zinc-800 p-4 flex flex-col gap-3'>
-			<h3 className='flex items-center'>{habit.name}</h3>
-			<p>{habit.description}</p>
-		</li>
-	);
-}
 
 export default HabitList;
